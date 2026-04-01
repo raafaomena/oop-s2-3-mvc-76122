d17b6ef8093278edd3da0d281bd4f481d62007f1
Rafaela Omena <rafaomena.s@gmail.com>
2026-04-01 11:54:25 +0100

    feat: add Branch entity

---
+++ VgcCollege.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace oop_s2_3_mvc_76122.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
---
+++ VgcCollege.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace oop_s2_3_mvc_76122.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
